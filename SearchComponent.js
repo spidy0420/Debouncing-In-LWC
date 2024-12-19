0c1aff50217b12e58cb91e26ab3072c46c4da754
spidy0420 <70722990+spidy0420@users.noreply.github.com>
2024-12-20 04:18:23 +0530

    Update SearchComponent.js

--- SearchComponent.js
+++ SearchComponent.js
@@ -1,35 +1,33 @@
 // SearchComponent.js
 import { LightningElement } from 'lwc';
+import fetchData from '@salesforce/apex/SearchComponentController.fetchData';
 
 export default class SearchComponent extends LightningElement {
-    searchTerm = '';
-
-    // Called when the component is initialized
-    connectedCallback() {
-        // Bind the debounced search function
-        this.handleSearchDebounced = this.debounce(this.handleSearch, 300);
-    }
-
-    // Handles input change and triggers debounced search
+    searchKey = '';
+    apiFilter = '';
     handleInputChange(event) {
-        this.searchTerm = event.target.value;
-        this.handleSearchDebounced();
+        // Update searchKey value on input change
+        this.searchKey = event.target.value;
+        this.debounceSearch();
     }
 
-    // Debounce function to limit search execution frequency
-    debounce(func, delay) {
-        let timeoutId;
-        return (...args) => {
-            clearTimeout(timeoutId);
-            timeoutId = setTimeout(() => {
-                func.apply(this, args);
-            }, delay);
-        };
+    @wire(fetchData, { apiFilter: '$apiFilter' })
+    wiredData({ error, data }) {
+        if (data) {
+            // Process fetched data
+        } else if (error) {
+            // Handle error
+        }
     }
 
-    // Your search logic
-    handleSearch() {
-        console.log('Searching for:', this.searchTerm);
-        // Add search logic here
+    debounceSearch() {
+        if (this.debounceTimeout) {
+            clearTimeout(this.debounceTimeout);
+        }
+        // Debounce API call by 1500ms
+        this.debounceTimeout = setTimeout(() => {
+          // Update the api search filter once each 1.5 seconds
+          this.apiFilter = this.searchKey;
+        }, 1500);
     }
 }
